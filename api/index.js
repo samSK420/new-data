64d3845b6f99f77b948a914c56c226d900f81b89
samSK420 <122343755+samSK420@users.noreply.github.com>
2023-01-10 16:29:48 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.require("db.json");
+const middleWares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+
+
+server.use(middleWares);
+server.use(router);
+server.listen(port);
+
+
+
+
+
